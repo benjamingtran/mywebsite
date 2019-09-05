484d3552e9e3bd4ba1770ff5975de9e88924117e
benjamingtran <benjaminghtran@gmail.com>
2019-09-05 14:59:32 -0700

    Updates

---
+++ client.js
@@ -0,0 +1,54 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var month = today.getMonth();
+    var day = today.getDay() + 1;
+    var year = today.getFullYear();
+    month = displayMonth(month);
+    var date = month +" "+ day +", " + year;
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s;
+    document.getElementById('date').innerHTML =
+         date;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    return i;
+}
+function displayMonth(month){
+    switch (month) {
+        case 1:
+            return "January";
+        case 2:
+            return "February";
+        case 3:
+            return "March";
+        case 4:
+            return "April";
+        case 5:
+            return "May";
+        case 6:
+            return "June";
+        case 7:
+            return "July";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "October";
+        case 11:
+            return "November";
+        case 12:
+            return "December";
+    }
+}
+function toggleMenu()
+{
+    document.getElementById("sidebar").classList.toggle("active");
+}
